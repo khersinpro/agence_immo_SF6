2df97f1e7dbebb763d99c24c5bac094b24eef74f
khersinpro <khersinpro@gmail.com>
2023-01-09 14:21:02 +0100

    add webpack , jquery, pm2, sass, sassloader and configure webpack

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+import './styles/app.scss';
+require('bootstrap');
+const $ = require('jquery');
+require('select2');
+
+$('select').select2();
+
+
+
+
+
--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
