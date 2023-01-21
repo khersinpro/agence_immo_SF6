c0ea77993c29d1eaabfe5f9f1553f1d670bea0ff
khersinpro <khersinpro@gmail.com>
2023-01-21 11:34:31 +0100

    add full admin dashboard

--- assets/app.js
+++ assets/app.js
@@ -38,4 +38,10 @@ window.addEventListener('load', () => {
     document.getElementById('admin-nav-toggler').addEventListener('click', () => {
         document.getElementById('admin-navbar').classList.toggle('toggle');
     })
+
+    window.addEventListener('resize', () => {
+        if (document.getElementById('admin-navbar').classList.contains('toggle')) {
+            document.getElementById('admin-navbar').classList.remove('toggle');
+        }
+    })
 })
